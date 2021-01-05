d0bde278beab724850fbfdd3b79ede38111cd799
Lawrence Nseir <lawrence.nseir@gmail.com>
2021-01-05 22:55:34 +0100

    feat: support wording in header cell

--- projects/im-grid/src/lib/pipes/translate.pipe.ts
+++ projects/im-grid/src/lib/pipes/translate.pipe.ts
@@ -5,13 +5,16 @@ import { Translation } from '../models/settings.model';
 import { SettingsService } from '../services/settings.service';
 
 @Pipe({
-    name: 'translate'
+    name: 'translate',
 })
 export class TranslatePipe implements PipeTransform {
-    constructor(private settingsService: SettingsService) { }
+    constructor(private settingsService: SettingsService) {}
 
     transform(object: Translation): Observable<string> {
-        return this.settingsService.settings
-            .pipe(map((settings) => object[settings.language]));
+        return this.settingsService.settings.pipe(
+            map((settings) =>
+                typeof object === 'string' ? object : object[settings.language]
+            )
+        );
     }
 }
