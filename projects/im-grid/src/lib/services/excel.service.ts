361e887ac02db30ca3ba50bd69799a7fdd15b51c
Lawrence Nseir <lawrence.nseir@gmail.com>
2021-01-05 23:19:53 +0100

    feat: support title translations everywhere

--- projects/im-grid/src/lib/services/excel.service.ts
+++ projects/im-grid/src/lib/services/excel.service.ts
@@ -3,21 +3,31 @@ import { utils, WorkBook, WorkSheet, write } from 'xlsx';
 import { DOCUMENT } from '@angular/common';
 import { ImColumn, ImColumnType } from '../models/column.model';
 import { FormatService } from './format.service';
-const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
+import { SettingsService } from './settings.service';
+const EXCEL_TYPE =
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 
 @Injectable({ providedIn: 'root' })
 export class ExcelService {
     constructor(
         private formatService: FormatService,
+        private settingsService: SettingsService,
         @Inject(DOCUMENT) private document: any
-    ) { }
+    ) {}
 
-    public exportAsExcelFile(rows: any[], excelFileName: string, columns: ImColumn[]): void {
+    public exportAsExcelFile(
+        rows: any[],
+        excelFileName: string,
+        columns: ImColumn[]
+    ): void {
         const rowsToExport = this.formatedRows(rows, columns);
         const worksheet: WorkSheet = utils.json_to_sheet(rowsToExport);
 
-        worksheet['!cols'] = columns.map(column => ({ wpx: column.width / 1.2 }));
-        const workbook: WorkBook = { Sheets: { ['data']: worksheet }, SheetNames: ['data'] };
+        worksheet['!cols'] = columns.map((column) => ({ wpx: column.width / 1.2 }));
+        const workbook: WorkBook = {
+            Sheets: { ['data']: worksheet },
+            SheetNames: ['data'],
+        };
         const excelBuffer: any = write(workbook, { bookType: 'xlsx', type: 'array' });
         this.saveAsExcelFile(excelBuffer, excelFileName);
     }
@@ -38,14 +48,13 @@ export class ExcelService {
         const formatedRows: any[] = [];
         const clonedRows = JSON.parse(JSON.stringify(rows));
 
-        clonedRows.forEach(row => {
+        clonedRows.forEach((row) => {
             const toFormatRow = {};
-            columns.forEach(column => {
-
+            columns.forEach((column) => {
                 const value = this.formatValue(column, row[column.key]);
                 const type = this.getType(column, value);
 
-                toFormatRow[column.title] = {
+                toFormatRow[column.title[this.settingsService.language]] = {
                     v: value,
                     t: type,
                 };
@@ -65,7 +74,8 @@ export class ExcelService {
             case ImColumnType.Object:
             case ImColumnType.Array:
                 return this.formatService.format(value, column);
-            default: return value;
+            default:
+                return value;
         }
     }
 
@@ -76,10 +86,14 @@ export class ExcelService {
 
         switch (column.columnType) {
             case ImColumnType.Int:
-            case ImColumnType.Decimal: return 'n';
-            case ImColumnType.Date: return 'd';
-            case ImColumnType.Boolean: return 'b';
-            default: return 's';
+            case ImColumnType.Decimal:
+                return 'n';
+            case ImColumnType.Date:
+                return 'd';
+            case ImColumnType.Boolean:
+                return 'b';
+            default:
+                return 's';
         }
     }
 }
