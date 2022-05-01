60275803f3d0e964e29d68b6c5c24603b41f40cd
Lawrence Nseir <lawrence.nseir@gmail.com>
2022-05-01 15:29:02 +0200

    refactor: remove observable translation

--- projects/im-grid/src/lib/pipes/translate.pipe.ts
+++ projects/im-grid/src/lib/pipes/translate.pipe.ts
@@ -1,6 +1,4 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 import { Translation } from '../models/settings.model';
 import { SettingsService } from '../services/settings.service';
 
@@ -10,11 +8,9 @@ import { SettingsService } from '../services/settings.service';
 export class TranslatePipe implements PipeTransform {
     constructor(private settingsService: SettingsService) {}
 
-    transform(object: string | Translation): Observable<string> {
-        return this.settingsService.settings.pipe(
-            map((settings) =>
-                typeof object === 'string' ? object : object[settings.language]
-            )
-        );
+    transform(translation: Translation | string): string {
+        return typeof translation === 'string'
+            ? translation
+            : translation[this.settingsService.language];
     }
 }
